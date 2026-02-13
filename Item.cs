856ce93f00e2775132f1575d15bf5e47f8361d1a
zenin5479 <zenin5479@gmail.com>
2026-02-13 09:19:45 +0300

    Update 3.2 Обновлен метод. Проведен рефакторинг

--- Item.cs
+++ Item.cs
@@ -1,7 +1,14 @@
-﻿namespace SyncServerWinForms
+﻿using System;
+
+namespace SyncServerWinForms
 {
    public class Item
    {
-      
+      public DateTimeOffset Date { get; set; }
+      public long Timestamp { get; set; }
+      public int Id { get; set; }
+      public string Vendor { get; set; }
+      public string Name { get; set; }
+      public double Price { get; set; }
    }
 }
