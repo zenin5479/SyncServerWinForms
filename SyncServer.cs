87237b4fa0d1411d93cae53581eab316e4f0a432
zenin5479 <zenin5479@gmail.com>
2026-02-14 11:32:53 +0300

    Update 9.2 Обновлен метод. Проведен рефакторинг

--- SyncServer.cs
+++ SyncServer.cs
@@ -23,7 +23,7 @@ namespace SyncServerWinForms
       }
 
       // Запись логов
-      private void Savelog(string tolog, Color color, RichTextBox richTextBoxSavelog, TextBox textBoxReader)
+      private void Savelog(string tolog, Color color, RichTextBox richTextBoxSavelog, TextBox textBoxReader, ListBox ListBoxReader)
       {
          richTextBoxSavelog.SelectionColor = color;
          richTextBoxSavelog.AppendText(tolog);
