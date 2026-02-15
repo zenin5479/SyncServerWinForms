d100d40796bc2feb1d5e5f12dbecba38cbd5e02c
zenin5479 <zenin5479@gmail.com>
2026-02-15 10:25:38 +0300

    Update 11.6 Обновлен метод. Проведен рефакторинг

--- SyncServer.cs
+++ SyncServer.cs
@@ -14,7 +14,7 @@ namespace SyncServerWinForms
       private readonly HttpListener _listener;
       private readonly string _url;
       private readonly TextBox _textBoxReader;
-      private readonly ListBox listBoxReader;
+      private readonly ListBox _listBoxReader;
       private readonly RichTextBox richTextBoxReader;
       private readonly List<Item> _items = new List<Item>();
 
@@ -22,7 +22,7 @@ namespace SyncServerWinForms
       {
          _url = url;
          _textBoxReader = textBoxReader;
-         this.listBoxReader = listBoxReader;
+         this._listBoxReader = listBoxReader;
          this.richTextBoxReader = richTextBoxReader;
          _listener = new HttpListener();
          _listener.Prefixes.Add(url);
