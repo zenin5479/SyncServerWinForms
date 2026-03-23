93c3e06fda3c111b8dda2c093c7d7346fc45d59b
zenin5479 <zenin5479@gmail.com>
2026-03-23 15:12:58 +0300

    Update 19.3 Обновлен метод. Проведен рефакторинг

--- SyncServer.cs
+++ SyncServer.cs
@@ -84,7 +84,7 @@ namespace SyncServerWinForms
          }
          catch (Exception ex)
          {
-            //_textBoxReader.AppendText("Ошибка: " + ex.Message);
+            _textBoxReader.AppendText("Ошибка: " + ex.Message);
          }
       }
 
