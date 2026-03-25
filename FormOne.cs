766edd942e8e253dc2f314c1321256678ce7cebd
zenin5479 <zenin5479@gmail.com>
2026-03-25 15:17:02 +0300

    Update 23.6 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -67,7 +67,7 @@ namespace SyncServerWinForms
 
       private void ButtonStop_Click(object sender, EventArgs e)
       {
-         //SyncServer.Stop(); //
+         SyncServer.Stop();
       }
    }
 }
