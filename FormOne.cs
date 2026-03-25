bdc9bce404185c820ce729b2b2ebad58c9ccc740
zenin5479 <zenin5479@gmail.com>
2026-03-25 09:51:15 +0300

    Update 22.9 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -21,7 +21,7 @@ namespace SyncServerWinForms
 
          ListBoxReader.Items.Add(lineone);
 
-         RichTextBoxReader.AppendText();
+         RichTextBoxReader.AppendText(lineone);
 
          string url = "http://127.0.0.1:8080/";
          // Создаём экземпляр класса и передаём ему ссылки на элементы управления
