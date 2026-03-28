b08fa5a19df1b5157e50af4aea07416412803aa3
zenin5479 <zenin5479@gmail.com>
2026-03-28 12:18:13 +0300

    Update 29.3 Обновлен метод. Проведен рефакторинг

--- SyncServer.cs
+++ SyncServer.cs
@@ -83,13 +83,9 @@ namespace SyncServerWinForms
                _textBoxReader.AppendText(Environment.NewLine);
 
                _listBoxReader.Items.Add("Запросы к серверу завершены");
-               // Прокрутка ListBox вниз
-               _listBoxReader.TopIndex = _listBoxReader.Items.Count - 1;
 
                _richTextBoxReader.AppendText("Запросы к серверу завершены");
                _richTextBoxReader.AppendText(Environment.NewLine);
-               // Прокрутка RichTextBox вниз
-               _richTextBoxReader.ScrollToCaret();
             }
          }
          catch (Exception ex)
